3a9c9e6fd488606050b124c0ca2c53f1a3dca4df
Tobias Knerr <osm@tobias-knerr.de>
2011-04-13 17:02:45 +0200

    Mac OS X support in start script

--- build-adds/osm2world.sh
+++ build-adds/osm2world.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-  java -Djava.library.path="lib/jogl/linux-amd64" -Xmx2G -jar OSM2World.jar $*
+KERNEL_NAME=`uname -s`
+
+if [ ${KERNEL_NAME} == 'Darwin' ]; then
+  java -Djava.library.path="lib/jogl/macosx-universal" -Xmx2G -jar OSM2World.jar $*
 else
-  java -Djava.library.path="lib/jogl/linux-i586" -Xmx2G -jar OSM2World.jar $*
+  if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    java -Djava.library.path="lib/jogl/linux-amd64" -Xmx2G -jar OSM2World.jar $*
+  else
+    java -Djava.library.path="lib/jogl/linux-i586" -Xmx2G -jar OSM2World.jar $*
+  fi
 fi
