f7ed3676651a053c00bc1d5af4b69db913b59ce5
Tobias Knerr <osm@tobias-knerr.de>
2012-05-14 00:43:35 +0200

    avoid creating a Tree object for each tree in a forest

--- src/org/osm2world/core/world/modules/TreeModule.java
+++ src/org/osm2world/core/world/modules/TreeModule.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.nCopies;
 import static org.osm2world.core.math.VectorXYZ.Y_UNIT;
 import static org.osm2world.core.world.modules.common.WorldModuleGeometryUtil.filterWorldObjectCollisions;
+import static org.osm2world.core.world.modules.common.WorldModuleParseUtil.parseHeight;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -35,7 +36,6 @@ import org.osm2world.core.world.data.NodeWorldObject;
 import org.osm2world.core.world.data.WaySegmentWorldObject;
 import org.osm2world.core.world.data.WorldObject;
 import org.osm2world.core.world.modules.common.ConfigurableWorldModule;
-import org.osm2world.core.world.modules.common.WorldModuleParseUtil;
 
 /**
  * adds trees, tree rows, tree groups and forests to the world
@@ -56,7 +56,7 @@ public class TreeModule extends ConfigurableWorldModule {
 		for (MapNode node : mapData.getMapNodes()) {
 			
 			if (node.getTags().contains("natural", "tree")) {
-				node.addRepresentation(new Tree(node, node.getPos()));
+				node.addRepresentation(new Tree(node));
 			}
 			
 		}
@@ -80,47 +80,169 @@ public class TreeModule extends ConfigurableWorldModule {
 		}
 		
 	}
+
+	private static final float DEFAULT_TREE_HEIGHT = 10;
+	private static final float TREE_RADIUS_PER_HEIGHT = 0.2f;
+	
+	private void renderTree(Target<?> target,
+			MapElement element,	VectorXZ pos) {
+		
+		VectorXYZ posXYZ = element.getElevationProfile().getWithEle(pos);
+		boolean coniferous = isConiferousTree(element, pos);
+		double height = getTreeHeight(element);
+		
+		if (useBillboards) {
+			
+			Material material = coniferous
+					? Materials.TREE_BILLBOARD_CONIFEROUS
+					: Materials.TREE_BILLBOARD_BROAD_LEAVED;
+			
+			renderBillboard(target, material, posXYZ, 0.5*height, height);
+			
+		} else {
+			
+			renderTreeGeometry(target, posXYZ, coniferous, height);
+			
+		}
+		
+	}
+	
+	private static void renderTreeGeometry(Target<?> target,
+			VectorXYZ posXYZ, boolean coniferous, double height) {
+		
+		double stemRatio = coniferous?0.3:0.5;
+		double radius = height*TREE_RADIUS_PER_HEIGHT;
+		
+		target.drawColumn(Materials.TREE_TRUNK,
+				null, posXYZ, height*stemRatio,
+				radius / 4, radius / 5, false, true);
+		
+		target.drawColumn(Materials.TREE_CROWN,
+				null, posXYZ.y(posXYZ.y+height*stemRatio),
+				height*(1-stemRatio),
+				radius,
+				coniferous ? 0 : radius,
+				true, true);
+	}
+	
+	private static void renderBillboard(Target<?> target,
+			Material material, VectorXYZ pos,
+			double width, double height) {
+		
+		//TODO use camera position
+		
+		VectorXYZ halfRight = VectorXYZ.X_UNIT.mult(0.5 * width);
+		VectorXYZ up = Y_UNIT.mult(height);
+		
+		VectorXYZ rightBottom = pos.add(halfRight);
+		VectorXYZ leftBottom = pos.subtract(halfRight);
+		
+		target.drawTriangleStrip(material, asList(
+				leftBottom.add(up), leftBottom,
+				rightBottom.add(up), rightBottom),
+				nCopies(material.getTextureDataList().size(),
+						BILLBOARD_TEX_COORDS));
+		
+	}
 	
 	private static final List<VectorXZ> BILLBOARD_TEX_COORDS = asList(
 			VectorXZ.Z_UNIT, VectorXZ.NULL_VECTOR,
 			new VectorXZ(1, 1), VectorXZ.X_UNIT);
+	
+	private static boolean isConiferousTree(MapElement element, VectorXZ pos) {
+		
+		String typeValue = element.getTags().getValue("wood");
+		
+		if (typeValue == null) {
+			typeValue = element.getTags().getValue("type");
+		}
+		
+		if ("broad_leaved".equals(typeValue)
+				|| "broad_leafed".equals(typeValue) // both values are common
+				|| "deciduous".equals(typeValue)) {
+			return false;
+		} else if ("coniferous".equals(typeValue)) {
+			return true;
+		} else { //mixed or undefined
+			//"random" decision based on x coord
+			return (long)(pos.getX()) % 2 == 0;
+		}
+		
+	}
+	
+	/**
+	 * parse height (for forests, add some random factor)
+	 */
+	private static double getTreeHeight(MapElement element) {
+		
+		float heightFactor = 1;
+		if (element.getTags().contains("natural", "wood")
+				|| element.getTags().contains("landuse", "forest")) {
+			heightFactor += -0.25f + 0.5f * Math.random();
+		}
+		
+		return heightFactor *
+				parseHeight(element.getTags(), DEFAULT_TREE_HEIGHT);
+		
+	}
+	
+	private POVRayTarget previousDeclarationTarget = null;
+	
+	private void addTreeDeclarationsTo(POVRayTarget target) {
+		if (target != previousDeclarationTarget) {
+		
+			previousDeclarationTarget = target;
+			
+			target.append("#ifndef (broad_leaved_tree)\n");
+			target.append("#declare broad_leaved_tree = object { union {\n");
+			renderTreeGeometry(target, VectorXYZ.NULL_VECTOR, false, 1);
+			target.append("} }\n#end\n\n");
+			
+			target.append("#ifndef (coniferous_tree)\n");
+			target.append("#declare coniferous_tree = object { union {\n");
+			renderTreeGeometry(target, VectorXYZ.NULL_VECTOR, true, 1);
+			target.append("} }\n#end\n\n");
+			
+		}
+	}
 
-	
-	
-	private static POVRayTarget previousDeclarationTarget = null;
+	private void renderTree(POVRayTarget target,
+			MapElement element, VectorXZ pos) {
+		
+		double height = getTreeHeight(element);
+		
+		//rotate randomly for variation
+		float yRotation = (float) Math.random() * 360;
+		
+		//add union of stem and leaves
+		if (isConiferousTree(element, pos)) {
+			target.append("object { coniferous_tree rotate ");
+		} else {
+			target.append("object { broad_leaved_tree rotate ");
+		}
+		
+		target.append(Float.toString(yRotation));
+		target.append("*y scale ");
+		target.append(height);
+		target.append(" translate ");
+		target.appendVector(pos.x, 0, pos.z);
+		target.append(" }\n");
+		
+	}
 	
 	private class Tree
 		implements NodeWorldObject, IntersectionTestObject,
 		RenderableToAllTargets, RenderableToPOVRay {
-
-		private static final float DEFAULT_HEIGHT = 10;
-		private static final float RADIUS_PER_HEIGHT = 0.2f;
 		
-		private final MapElement element;
-		private final VectorXZ pos;
-		private final float height;
+		private final MapNode node;
 
-		public Tree(MapElement element, VectorXZ pos) {
-
-			this.element = element;
-			this.pos = pos;
-			
-			/* parse height (for forests, add some random factor) */
-			
-			float heightFactor = 1;
-			if (element.getTags().contains("natural", "wood")
-					|| element.getTags().contains("landuse", "forest")) {
-				heightFactor += -0.25f + 0.5f * Math.random();
-			}
-			
-			this.height = heightFactor *
-				WorldModuleParseUtil.parseHeight(element.getTags(), DEFAULT_HEIGHT);
-			
+		public Tree(MapNode node) {
+			this.node = node;
 		}
 		
 		@Override
 		public MapElement getPrimaryMapElement() {
-			return element;
+			return node;
 		}
 		
 		@Override
@@ -130,7 +252,7 @@ public class TreeModule extends ConfigurableWorldModule {
 		
 		@Override
 		public double getClearingAbove(VectorXZ pos) {
-			return height;
+			return getTreeHeight(node);
 		}
 		
 		@Override
@@ -140,137 +262,29 @@ public class TreeModule extends ConfigurableWorldModule {
 		
 		@Override
 		public AxisAlignedBoundingBoxXZ getAxisAlignedBoundingBoxXZ() {
-			return new AxisAlignedBoundingBoxXZ(Collections.singleton(pos));
+			return new AxisAlignedBoundingBoxXZ(Collections.singleton(node.getPos()));
 		}
 		
 		@Override
 		public void renderTo(Target<?> target) {
-			
-			VectorXYZ posXYZ = element.getElevationProfile().getWithEle(pos);
-			
-			if (useBillboards) {
-				
-				Material material = isConiferous()
-						? Materials.TREE_BILLBOARD_CONIFEROUS
-						: Materials.TREE_BILLBOARD_BROAD_LEAVED;
-				
-				renderBillboard(target, material, posXYZ, 0.5*height, height);
-				
-			} else {
-				
-				renderTree(target, posXYZ, isConiferous(), height);
-				
-			}
-			
+			renderTree(target, node, node.getPos());
 		}
 		
 		@Override
 		public void addDeclarationsTo(POVRayTarget target) {
-
-			if (target != previousDeclarationTarget) {
-			
-				previousDeclarationTarget = target;
-				
-				target.append("#ifndef (broad_leaved_tree)\n");
-				target.append("#declare broad_leaved_tree = object { union {\n");
-				renderTree(target, VectorXYZ.NULL_VECTOR, false, 1);
-				target.append("} }\n#end\n\n");
-				
-				target.append("#ifndef (coniferous_tree)\n");
-				target.append("#declare coniferous_tree = object { union {\n");
-				renderTree(target, VectorXYZ.NULL_VECTOR, true, 1);
-				target.append("} }\n#end\n\n");
-				
-			}
-			
+			addTreeDeclarationsTo(target);
 		}
 		
 		@Override
 		public void renderTo(POVRayTarget target) {
-			
-			//rotate randomly for variation
-			float yRotation = (float) Math.random() * 360;
-			
-			//add union of stem and leaves
-			if (isConiferous()) {
-				target.append("object { coniferous_tree rotate ");
-			} else {
-				target.append("object { broad_leaved_tree rotate ");
-			}
-			
-			target.append(Float.toString(yRotation));
-			target.append("*y scale ");
-			target.append(height);
-			target.append(" translate ");
-			target.appendVector(pos.x, 0, pos.z);
-			target.append(" }\n");
-			
-		}
-
-		private void renderTree(Target<?> target,
-				VectorXYZ posXYZ, boolean coniferous, double height) {
-			
-			double stemRatio = coniferous?0.3:0.5;
-			double radius = height*RADIUS_PER_HEIGHT;
-			
-			target.drawColumn(Materials.TREE_TRUNK,
-					null, posXYZ, height*stemRatio,
-					radius / 4, radius / 5, false, true);
-			
-			target.drawColumn(Materials.TREE_CROWN,
-					null, posXYZ.y(posXYZ.y+height*stemRatio),
-					height*(1-stemRatio),
-					radius,
-					coniferous ? 0 : radius,
-					true, true);
-		}
-		
-		private void renderBillboard(Target<?> target,
-				Material material, VectorXYZ pos,
-				double width, double height) {
-			
-			//TODO use camera position
-			
-			VectorXYZ halfRight = VectorXYZ.X_UNIT.mult(0.5 * width);
-			VectorXYZ up = Y_UNIT.mult(height);
-			
-			VectorXYZ rightBottom = pos.add(halfRight);
-			VectorXYZ leftBottom = pos.subtract(halfRight);
-			
-			target.drawTriangleStrip(material, asList(
-					leftBottom.add(up), leftBottom,
-					rightBottom.add(up), rightBottom),
-					nCopies(material.getTextureDataList().size(),
-							BILLBOARD_TEX_COORDS));
-			
-		}
-		
-		private boolean isConiferous() {
-			
-			String typeValue = element.getTags().getValue("wood");
-			
-			if (typeValue == null) {
-				typeValue = element.getTags().getValue("type");
-			}
-			
-			if ("broad_leaved".equals(typeValue)
-					|| "broad_leafed".equals(typeValue) // both values are common
-					|| "deciduous".equals(typeValue)) {
-				return false;
-			} else if ("coniferous".equals(typeValue)) {
-				return true;
-			} else { //mixed or undefined
-				//"random" decision based on x coord
-				return (long)(pos.getX()) % 2 == 0;
-			}
-			
+			renderTree(target, node, node.getPos());
 		}
 		
 	}
 
 	private class TreeRow implements WaySegmentWorldObject, RenderableToAllTargets, RenderableToPOVRay {
 
-		private final List<Tree> trees;
+		private final Collection<VectorXZ> treePositions;
 		private final MapWaySegment line;
 		
 		public TreeRow(MapWaySegment line) {
@@ -279,17 +293,11 @@ public class TreeModule extends ConfigurableWorldModule {
 			
 			//TODO: spread along a full way
 			
-			List<VectorXZ> treePositions = GeometryUtil.equallyDistributePointsAlong(
+			treePositions = GeometryUtil.equallyDistributePointsAlong(
 					4 /* TODO: derive from tree count */ ,
 					false /* TODO: should be true once a full way is covered */,
 					line.getStartNode().getPos(), line.getEndNode().getPos());
-			
-			trees = new ArrayList<Tree>(treePositions.size());
-			
-			for (VectorXZ treePos : treePositions) {
-				trees.add(new Tree(line, treePos));
-			}
-			
+						
 		}
 		
 		@Override
@@ -324,18 +332,20 @@ public class TreeModule extends ConfigurableWorldModule {
 
 		@Override
 		public void renderTo(Target<?> target) {
-			for (Tree tree : trees) {
-				tree.renderTo(target);
+			for (VectorXZ pos : treePositions) {
+				renderTree(target, line, pos);
 			}
 		}
 
 		@Override
-		public void addDeclarationsTo(POVRayTarget target) {}
+		public void addDeclarationsTo(POVRayTarget target) {
+			addTreeDeclarationsTo(target);
+		}
 		
 		@Override
 		public void renderTo(POVRayTarget target) {
-			for (Tree tree : trees) {
-				tree.renderTo(target);
+			for (VectorXZ pos : treePositions) {
+				renderTree(target, line, pos);
 			}
 		}
 		
@@ -348,14 +358,14 @@ public class TreeModule extends ConfigurableWorldModule {
 		private final MapArea area;
 		private final MapData mapData;
 		
-		private Collection<Tree> trees = null;
+		private Collection<VectorXZ> treePositions = null;
 		
 		public Forest(MapArea area, MapData mapData) {
 			this.area = area;
 			this.mapData = mapData;
 		}
 
-		private void createTrees(double density) {
+		private void createTreePositions(double density) {
 			
 			/* collect other objects that the trees should not be placed on */
 			
@@ -373,19 +383,13 @@ public class TreeModule extends ConfigurableWorldModule {
 			
 			/* place the trees */
 			
-			List<VectorXZ> treePositions =
+			treePositions =
 				GeometryUtil.distributePointsOn(area.getOsmObject().id,
 						area.getPolygon(), mapData.getBoundary(),
 						density, 0.3f);
 			
 			filterWorldObjectCollisions(treePositions, avoidedObjects);
-						
-			trees = new ArrayList<Tree>(treePositions.size());
-						
-			for (VectorXZ treePosition : treePositions) {
-				trees.add(new Tree(area, treePosition));
-			}
-			
+									
 		}
 		
 		@Override
@@ -410,25 +414,27 @@ public class TreeModule extends ConfigurableWorldModule {
 		
 		@Override
 		public void renderTo(POVRayTarget target) {
-			if (trees == null) {
-				createTrees(config.getDouble("treesPerSquareMeter", 0.01f));
+			if (treePositions == null) {
+				createTreePositions(config.getDouble("treesPerSquareMeter", 0.01f));
 			}
-			for (Tree tree : trees) {
-				tree.renderTo(target);
+			for (VectorXZ pos : treePositions) {
+				renderTree(target, area, pos);
 			}
 		}
 		
 		@Override
-		public void addDeclarationsTo(POVRayTarget target) {}
+		public void addDeclarationsTo(POVRayTarget target) {
+			addTreeDeclarationsTo(target);
+		}
 		
 		@Override
 		public void renderTo(Target<?> target) {
-			if (trees == null) {
-				createTrees(config.getDouble("treesPerSquareMeter", 0.001f));
+			if (treePositions == null) {
+				createTreePositions(config.getDouble("treesPerSquareMeter", 0.001f));
 					//lower default density than POVRay for performance reasons
 			}
-			for (Tree tree : trees) {
-				tree.renderTo(target);
+			for (VectorXZ pos : treePositions) {
+				renderTree(target, area, pos);
 			}
 		}
 		
