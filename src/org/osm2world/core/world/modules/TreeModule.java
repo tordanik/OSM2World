173106ef973bf615b90d49b91bb0b7c125cb9a42
Tobias Knerr <osm@tobias-knerr.de>
2012-07-07 21:00:22 +0200

    extract billboard rendering code from TreeModule into new utility class

--- src/org/osm2world/core/world/modules/TreeModule.java
+++ src/org/osm2world/core/world/modules/TreeModule.java
@@ -1,14 +1,11 @@
 package org.osm2world.core.world.modules;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.nCopies;
 import static org.osm2world.core.world.modules.common.WorldModuleGeometryUtil.filterWorldObjectCollisions;
 import static org.osm2world.core.world.modules.common.WorldModuleParseUtil.parseHeight;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 import org.apache.commons.configuration.Configuration;
 import org.openstreetmap.josm.plugins.graphview.core.data.Tag;
@@ -35,6 +32,7 @@ import org.osm2world.core.world.data.NodeWorldObject;
 import org.osm2world.core.world.data.WaySegmentWorldObject;
 import org.osm2world.core.world.data.WorldObject;
 import org.osm2world.core.world.modules.common.ConfigurableWorldModule;
+import org.osm2world.core.world.modules.common.WorldModuleBillboardUtil;
 
 /**
  * adds trees, tree rows, tree groups and forests to the world
@@ -103,7 +101,7 @@ public class TreeModule extends ConfigurableWorldModule {
 					? Materials.TREE_BILLBOARD_CONIFEROUS
 					: Materials.TREE_BILLBOARD_BROAD_LEAVED;
 			
-			renderBillboard(target, material, posXYZ,
+			WorldModuleBillboardUtil.renderCrosstree(target, material, posXYZ,
 					(fruit ? 1.0 : 0.5 ) * height, height, mirrored);
 			
 		} else {
@@ -131,141 +129,6 @@ public class TreeModule extends ConfigurableWorldModule {
 				coniferous ? 0 : radius,
 				true, true);
 	}
-	
-	private static void renderBillboard(Target<?> target,
-			Material material, VectorXYZ pos,
-			double width, double height,
-			boolean mirrored) {
-
-		double halfWidth = 0.5 * width;
-		
-		VectorXYZ xPosBottom = pos.add(halfWidth, 0, 0);
-		VectorXYZ xNegBottom = pos.add(-halfWidth, 0, 0);
-		VectorXYZ zPosBottom = pos.add(0, 0, halfWidth);
-		VectorXYZ zNegBottom = pos.add(0, 0, -halfWidth);
-
-		VectorXYZ xPosTop = xPosBottom.add(0, height, 0);
-		VectorXYZ xNegTop = xNegBottom.add(0, height, 0);
-		VectorXYZ zPosTop = zPosBottom.add(0, height, 0);
-		VectorXYZ zNegTop = zNegBottom.add(0, height, 0);
-		VectorXYZ posTop = pos.add(0, height, 0);
-		
-		target.drawTriangleStrip(material, asList(
-				xNegTop, xNegBottom, posTop, pos),
-				billboardTexCoordLists(false, mirrored,
-						material.getTextureDataList().size()));
-		
-		target.drawTriangleStrip(material, asList(
-				xPosBottom, xPosTop, pos, posTop),
-				billboardTexCoordLists(true, mirrored,
-						material.getTextureDataList().size()));
-
-		target.drawTriangleStrip(material, asList(
-				zNegTop, zNegBottom, posTop, pos),
-				billboardTexCoordLists(false, mirrored,
-						material.getTextureDataList().size()));
-		
-		target.drawTriangleStrip(material, asList(
-				zPosBottom, zPosTop, pos, posTop),
-				billboardTexCoordLists(true, mirrored,
-						material.getTextureDataList().size()));
-
-		target.drawTriangleStrip(material, asList(
-				xPosTop, xPosBottom, posTop, pos),
-				billboardTexCoordLists(false, mirrored,
-						material.getTextureDataList().size()));
-		
-		target.drawTriangleStrip(material, asList(
-				xNegBottom, xNegTop, pos, posTop),
-				billboardTexCoordLists(true, mirrored,
-						material.getTextureDataList().size()));
-
-		target.drawTriangleStrip(material, asList(
-				zPosTop, zPosBottom, posTop, pos),
-				billboardTexCoordLists(false, mirrored,
-						material.getTextureDataList().size()));
-		
-		target.drawTriangleStrip(material, asList(
-				zNegBottom, zNegTop, pos, posTop),
-				billboardTexCoordLists(true, mirrored,
-						material.getTextureDataList().size()));
-		
-	}
-	
-	private static List<List<VectorXZ>> billboardTexCoordLists(
-			boolean right, boolean mirrored, int copies) {
-		
-		if (right) {
-			
-			if (mirrored) {
-				if (copies <= 3) { return BILLBOARD_RIGHT_TEX_COORDS_MIRRORED_COPIES[copies]; }
-				else { return nCopies(copies, BILLBOARD_RIGHT_TEX_COORDS_MIRRORED); }
-			} else {
-				if (copies <= 3) { return BILLBOARD_RIGHT_TEX_COORDS_COPIES[copies]; }
-				else { return nCopies(copies, BILLBOARD_RIGHT_TEX_COORDS); }
-			}
-			
-		} else {
-			
-			if (mirrored) {
-				if (copies <= 3) { return BILLBOARD_LEFT_TEX_COORDS_MIRRORED_COPIES[copies]; }
-				else { return nCopies(copies, BILLBOARD_LEFT_TEX_COORDS_MIRRORED); }
-			} else {
-				if (copies <= 3) { return BILLBOARD_LEFT_TEX_COORDS_COPIES[copies]; }
-				else { return nCopies(copies, BILLBOARD_LEFT_TEX_COORDS); }
-			}
-			
-		}
-		
-	}
-	
-	private static final List<VectorXZ> BILLBOARD_LEFT_TEX_COORDS = asList(
-			VectorXZ.Z_UNIT, VectorXZ.NULL_VECTOR,
-			new VectorXZ(0.5, 1), new VectorXZ(0.5, 0));
-	
-	private static final List<VectorXZ> BILLBOARD_LEFT_TEX_COORDS_MIRRORED = asList(
-			new VectorXZ(1, 1), VectorXZ.X_UNIT,
-			new VectorXZ(0.5, 1), new VectorXZ(0.5, 0));
-		
-	private static final List<VectorXZ> BILLBOARD_RIGHT_TEX_COORDS = asList(
-			VectorXZ.X_UNIT, new VectorXZ(1, 1),
-			new VectorXZ(0.5, 0), new VectorXZ(0.5, 1));
-	
-	private static final List<VectorXZ> BILLBOARD_RIGHT_TEX_COORDS_MIRRORED = asList(
-			VectorXZ.NULL_VECTOR, VectorXZ.Z_UNIT,
-			new VectorXZ(0.5, 0), new VectorXZ(0.5, 1));
-	
-	@SuppressWarnings("unchecked")
-	private static final List<List<VectorXZ>>[] BILLBOARD_LEFT_TEX_COORDS_COPIES = new List[]{
-		nCopies(0, BILLBOARD_LEFT_TEX_COORDS),
-		nCopies(1, BILLBOARD_LEFT_TEX_COORDS),
-		nCopies(2, BILLBOARD_LEFT_TEX_COORDS),
-		nCopies(3, BILLBOARD_LEFT_TEX_COORDS)
-	};
-	
-	@SuppressWarnings("unchecked")
-	private static final List<List<VectorXZ>>[] BILLBOARD_LEFT_TEX_COORDS_MIRRORED_COPIES = new List[]{
-		nCopies(0, BILLBOARD_LEFT_TEX_COORDS_MIRRORED),
-		nCopies(1, BILLBOARD_LEFT_TEX_COORDS_MIRRORED),
-		nCopies(2, BILLBOARD_LEFT_TEX_COORDS_MIRRORED),
-		nCopies(3, BILLBOARD_LEFT_TEX_COORDS_MIRRORED)
-	};
-	
-	@SuppressWarnings("unchecked")
-	private static final List<List<VectorXZ>>[] BILLBOARD_RIGHT_TEX_COORDS_COPIES = new List[]{
-		nCopies(0, BILLBOARD_RIGHT_TEX_COORDS),
-		nCopies(1, BILLBOARD_RIGHT_TEX_COORDS),
-		nCopies(2, BILLBOARD_RIGHT_TEX_COORDS),
-		nCopies(3, BILLBOARD_RIGHT_TEX_COORDS)
-	};
-	
-	@SuppressWarnings("unchecked")
-	private static final List<List<VectorXZ>>[] BILLBOARD_RIGHT_TEX_COORDS_MIRRORED_COPIES = new List[]{
-		nCopies(0, BILLBOARD_RIGHT_TEX_COORDS_MIRRORED),
-		nCopies(1, BILLBOARD_RIGHT_TEX_COORDS_MIRRORED),
-		nCopies(2, BILLBOARD_RIGHT_TEX_COORDS_MIRRORED),
-		nCopies(3, BILLBOARD_RIGHT_TEX_COORDS_MIRRORED)
-	};
 		
 	private static boolean isConiferousTree(MapElement element, VectorXZ pos) {
 		
