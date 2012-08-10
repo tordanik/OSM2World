38f2e2c0a1583763991cad08c88904b628eb5ea7
Peter Barth <osm-peda@won2.de>
2012-08-10 15:50:34 +0200

    improve HackMapProjection and make it reversible

--- src/org/osm2world/core/map_data/creation/HackMapProjection.java
+++ src/org/osm2world/core/map_data/creation/HackMapProjection.java
@@ -1,13 +1,10 @@
 package org.osm2world.core.map_data.creation;
 
-import java.awt.geom.Point2D;
+import static org.osm2world.core.map_data.creation.MercatorProjection.*;
 
 import org.osm2world.core.math.VectorXZ;
 import org.osm2world.core.osm.data.OSMData;
 import org.osm2world.core.osm.data.OSMNode;
-
-import com.jhlabs.map.proj.MercatorProjection;
-import com.jhlabs.map.proj.Projection;
 
 /**
  * quick-and-dirty projection that is intended to use the "dense" space
@@ -16,64 +13,42 @@ import com.jhlabs.map.proj.Projection;
  */
 public class HackMapProjection implements MapProjection {
 	
-	private final Projection projection = new MercatorProjection();
+	/**
+	 * This is a correction value to make lonToX-values metric values
+	 */
+	private final static double LON_CORRECTION = 1.5;
 	
-	/** all coordinates will be modified by subtracting the first node's coords;
-	 *  this is supposed to keep nodes as close as possible to 0.0.
-	 *  (It also means that the first lat/lon values will be at 0.0)
-	 *  //TODO: re-evaluate this solution later */
-	private final Double firstNodeLat, firstNodeLon;
-
-	/* magic constant */
-	public static final double SCALE_X = 70000;
-	public static final double SCALE_Y = 110000;
+	/**
+	 * we remember the first lat/lon values so we can move all other
+	 * lat/lon-values near (0,0)
+	 */
+	private final double firstLat, firstLon;
 	
 	public HackMapProjection(OSMData osmData) {
 		if (osmData.getNodes().isEmpty()) {
 			throw new IllegalArgumentException("OSM data must contain nodes");
 		}
 		OSMNode firstNode = osmData.getNodes().iterator().next();
-		firstNodeLat = firstNode.lat;
-		firstNodeLon = firstNode.lon;
+		firstLat = firstNode.lat;
+		firstLon = firstNode.lon;
 	}
 
 	public VectorXZ calcPos(double lat, double lon) {
-				
-		lat -= firstNodeLat;
-		lon -= firstNodeLon;
 		
-//		return new VectorXZ(
-//				(float)conversion.MercatorProjection.lonToX(lon),
-//				(float)conversion.MercatorProjection.latToY(lat)
-//		);
+		double x = lonToX(lon - firstLon) / LON_CORRECTION;
+		double y = latToY(lat - firstLat);
 		
-		//TODO: maybe remove this projection code and the associated LIB?
-		
-		Point2D.Double point = new Point2D.Double();
-		if (Double.isNaN(projection.project(lon, lat, point).y * SCALE_Y)) {
-			System.out.println("NaN!");
-		}
-		projection.project(lon, lat, point);
-		return new VectorXZ(point.x * SCALE_X, point.y * SCALE_Y); //x and z(!) are 2d here
-		
+		return new VectorXZ(x, y); //x and z(!) are 2d here
 	}
 	
 	@Override
 	public double calcLat(VectorXZ pos) {
-		if (pos.equals(VectorXZ.NULL_VECTOR)) {
-			return firstNodeLat;
-		} else {
-			throw new UnsupportedOperationException();
-		}
+		return yToLat(pos.z) + firstLat;
 	}
 	
 	@Override
 	public double calcLon(VectorXZ pos) {
-		if (pos.equals(VectorXZ.NULL_VECTOR)) {
-			return firstNodeLon;
-		} else {
-			throw new UnsupportedOperationException();
-		}
+		return xToLon(pos.x * LON_CORRECTION) + firstLon;
 	}
 	
 	@Override
