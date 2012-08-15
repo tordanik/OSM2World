ab27593b3c693a670e1afcc2f4e497c67044752c
Tobias Knerr <osm@tobias-knerr.de>
2012-08-16 00:02:22 +0200

    restore functionality of those debug views that use PrimitiveBuffer

--- src/org/osm2world/viewer/view/debug/TerrainNormalsDebugView.java
+++ src/org/osm2world/viewer/view/debug/TerrainNormalsDebugView.java
@@ -2,7 +2,12 @@ package org.osm2world.viewer.view.debug;
 
 import java.awt.Color;
 
+import org.osm2world.core.math.VectorXYZ;
+import org.osm2world.core.target.common.Primitive;
+import org.osm2world.core.target.common.material.Material;
+import org.osm2world.core.target.common.material.Material.Lighting;
 import org.osm2world.core.target.jogl.JOGLTarget;
+import org.osm2world.core.target.jogl.PrimitiveBuffer;
 
 public class TerrainNormalsDebugView extends DebugView {
 
@@ -15,24 +20,33 @@ public class TerrainNormalsDebugView extends DebugView {
 	}
 	
 	@Override
+	public boolean canBeUsed() {
+		return terrain != null;
+	}
+	
+	@Override
 	protected void fillTarget(JOGLTarget target) {
 		
-//		for (Material material : terrainPrimitiveBuffer.getMaterials()) {
-//
-//			Color color = material.getLighting() == Lighting.FLAT ?
-//					FLAT_NORMALS_COLOR : SMOOTH_NORMALS_COLOR;
-//
-//			for (Primitive primitive : terrainPrimitiveBuffer.getPrimitives(material)) {
-//
-//				for (int i = 0; i < primitive.indices.length; i++) {
-//					int index = primitive.indices[i];
-//					VectorXYZ v = terrainPrimitiveBuffer.getVertex(index);
-//					target.drawArrow(color, 0.3f, v, v.add(primitive.normals.get(i)));
-//				}
-//
-//			}
-//
-//		}
+		PrimitiveBuffer terrainPrimitiveBuffer = new PrimitiveBuffer();
+		
+		terrain.renderTo(terrainPrimitiveBuffer);
+		
+		for (Material material : terrainPrimitiveBuffer.getMaterials()) {
+			
+			Color color = material.getLighting() == Lighting.FLAT ?
+					FLAT_NORMALS_COLOR : SMOOTH_NORMALS_COLOR;
+			
+			for (Primitive primitive : terrainPrimitiveBuffer.getPrimitives(material)) {
+				
+				for (int i = 0; i < primitive.vertices.size(); i++) {
+					VectorXYZ v = primitive.vertices.get(i);
+					VectorXYZ n = primitive.normals.get(i);
+					drawArrow(target, color, 0.3f, v, v.add(n));
+				}
+				
+			}
+			
+		}
 		
 	}
 
--- src/org/osm2world/viewer/view/debug/WorldObjectNormalsDebugView.java
+++ src/org/osm2world/viewer/view/debug/WorldObjectNormalsDebugView.java
@@ -1,8 +1,18 @@
 package org.osm2world.viewer.view.debug;
 
+import static org.osm2world.core.util.FaultTolerantIterationUtil.iterate;
+
 import java.awt.Color;
 
+import org.osm2world.core.math.VectorXYZ;
+import org.osm2world.core.target.TargetUtil;
+import org.osm2world.core.target.common.Primitive;
+import org.osm2world.core.target.common.material.Material;
+import org.osm2world.core.target.common.material.Material.Lighting;
 import org.osm2world.core.target.jogl.JOGLTarget;
+import org.osm2world.core.target.jogl.PrimitiveBuffer;
+import org.osm2world.core.util.FaultTolerantIterationUtil.Operation;
+import org.osm2world.core.world.data.WorldObject;
 
 public class WorldObjectNormalsDebugView extends DebugView {
 
@@ -17,28 +27,32 @@ public class WorldObjectNormalsDebugView extends DebugView {
 	@Override
 	protected void fillTarget(JOGLTarget target) {
 		
-//		for (Material material : mapDataPrimitiveBuffer.getMaterials()) {
-//
-//			Color color = material.getLighting() == Lighting.FLAT ?
-//					FLAT_NORMALS_COLOR : SMOOTH_NORMALS_COLOR;
-//
-//			// calculate some vectors for easily culling some of the vertices
-//			VectorXZ cam = camera.getPos().xz();
-//			VectorXZ rightOfCam = camera.getPos().add(camera.getRight()).xz();
-//
-//			for (Primitive primitive : mapDataPrimitiveBuffer.getPrimitives(material)) {
-//
-//				for (int i = 0; i < primitive.indices.length; i++) {
-//					int index = primitive.indices[i];
-//					VectorXYZ v = mapDataPrimitiveBuffer.getVertex(index);
-//					if (GeometryUtil.isRightOf(v.xz(), cam, rightOfCam)) {
-//						target.drawArrow(color, 0.3f, v, v.add(primitive.normals.get(i)));
-//					}
-//				}
-//
-//			}
-//
-//		}
+		final PrimitiveBuffer primitiveBuffer = new PrimitiveBuffer();
+
+		iterate(map.getWorldObjects(), new Operation<WorldObject>() {
+			@Override public void perform(WorldObject w) {
+				TargetUtil.renderObject(primitiveBuffer, w);
+			}
+		});
+		
+		for (Material material : primitiveBuffer .getMaterials()) {
+			
+			Color color = material.getLighting() == Lighting.FLAT ?
+					FLAT_NORMALS_COLOR : SMOOTH_NORMALS_COLOR;
+			
+			for (Primitive primitive : primitiveBuffer.getPrimitives(material)) {
+				
+				for (int i = 0; i < primitive.vertices.size(); i++) {
+					VectorXYZ v = primitive.vertices.get(i);
+					VectorXYZ n = primitive.normals.get(i);
+					if (n != null) {
+						drawArrow(target, color, 0.3f, v, v.add(n));
+					}
+				}
+				
+			}
+			
+		}
 		
 	}
 
