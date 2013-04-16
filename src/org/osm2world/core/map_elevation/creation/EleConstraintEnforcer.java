49c511937a57a03fe4e823eb448d6b871eb6e786
Tobias Knerr <osm@tobias-knerr.de>
2013-04-16 23:23:02 +0200

    write javadocs for EleConstraintEnforcer

--- src/org/osm2world/core/map_elevation/creation/EleConstraintEnforcer.java
+++ src/org/osm2world/core/map_elevation/creation/EleConstraintEnforcer.java
@@ -8,44 +8,50 @@ import org.osm2world.core.map_elevation.data.EleConnector;
 public interface EleConstraintEnforcer {
 	
 	/**
-	 * @param connectors  connector sets, with elevation values initially set to
+	 * makes connectors known to this enforcer. Only these connectors can be
+	 * used in constraints later on, and only they will be affected by
+	 * {@link #enforceConstraints()}.
+	 * 
+	 * @param connectors  connectors with elevation values initially set to
 	 *  terrain elevation at their xz position
 	 */
 	void addConnectors(Iterable<EleConnector> connectors);
 	
 	/**
-	 * TODO javadoc
+	 * requires two connectors to be at the same elevation
 	 */
 	public void addSameEleConstraint(EleConnector c1,
 			EleConnector c2);
 	
 	/**
-	 * TODO javadoc
+	 * requires a number of connectors to be at the same elevation
 	 */
 	public void addSameEleConstraint(Iterable<EleConnector> cs);
 	
 	/**
-	 * TODO javadoc
+	 * requires two connectors' elevations to differ at least by a given distance
 	 */
 	void addMinVerticalDistanceConstraint(
 			EleConnector upper, EleConnector lower, double distance);
 	
 	/**
-	 * TODO javadoc
-	 * @param cs
-	 * @param minIncline
+	 * requires a minimum incline along a sequence of connectors.
+	 * 
+	 * @param minIncline  incline value,
+	 *  negative values are inclines in opposite direction
 	 */
 	void addMinInclineConstraint(List<EleConnector> cs, double minIncline);
 	
 	/**
-	 * TODO javadoc
-	 * @param cs
-	 * @param maxIncline
+	 * requires a maximum incline along a sequence of connectors
+	 * 
+	 * @param maxIncline  incline value,
+	 *  negative values are inclines in opposite direction
 	 */
 	void addMaxInclineConstraint(List<EleConnector> cs, double maxIncline);
 	
 	/**
-	 * TODO javadoc
+	 * makes sure that there is a "smooth" transition between two line segments
 	 */
 	void addSmoothnessConstraint(EleConnector c2, EleConnector c1, EleConnector c3);
 	
