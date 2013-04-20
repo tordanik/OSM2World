6513e506eab5607f7a873ca022b3c20a15b5298f
Tobias Knerr <osm@tobias-knerr.de>
2013-04-20 18:35:54 +0200

    add special handling for highway=platform to default lane layout, fixes #51

--- src/org/osm2world/core/world/modules/RoadModule.java
+++ src/org/osm2world/core/world/modules/RoadModule.java
@@ -149,7 +149,8 @@ public class RoadModule extends ConfigurableWorldModule {
 				|| "service".equals(highwayValue)
 				|| "track".equals(highwayValue)
 				|| "residential".equals(highwayValue)
-				|| "pedestrian".equals(highwayValue)) {
+				|| "pedestrian".equals(highwayValue)
+				|| "platform".equals(highwayValue)) {
 			return 1;
 		} else if ("motorway".equals(highwayValue)){
 			return 2;
