bfe73604512b0a46bcab54d26dfd9768225d4522
Tobias Knerr <osm@tobias-knerr.de>
2013-06-30 20:35:54 +0200

    add a platform switch to osm2world.sh for lp_solve

--- build-adds/osm2world.sh
+++ build-adds/osm2world.sh
@@ -10,6 +10,23 @@ if [[ $1 == --vm-params=* ]]
     shift
 fi
 
+# choose path for the native JOGL libs depending on system and java version
+
+MACHINE_TYPE=`uname -m`
+KERNEL_NAME=`uname -s`
+
+if [ ${KERNEL_NAME} == 'Darwin' ]; then
+lpsolvepath="lib/lp_solve/mac"
+else
+if [ `java -version 2>&1|grep -c 64-Bit` -gt 0 ]; then
+lpsolvepath="lib/lp_solve/ux64"
+  else
+lpsolvepath="lib/lp_solve/ux32"
+  fi
+fi
+
 # run OSM2World
 
-java $vmparams -jar OSM2World.jar $@
+export LD_LIBRARY_PATH=$lpsolvepath
+
+java -Djava.library.path=$lpsolvepath $vmparams -jar OSM2World.jar --config texture_config.properties --gui $@
