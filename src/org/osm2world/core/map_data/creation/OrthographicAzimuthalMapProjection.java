3ab64c505518202d26f323eda604b037b68374c0
Tobias Knerr <osm@tobias-knerr.de>
2015-06-30 23:23:42 +0200

    fix OrthographicAzimuthalMapProjection: missing conversions between degrees and radians

--- src/org/osm2world/core/map_data/creation/OrthographicAzimuthalMapProjection.java
+++ src/org/osm2world/core/map_data/creation/OrthographicAzimuthalMapProjection.java
@@ -1,8 +1,10 @@
 package org.osm2world.core.map_data.creation;
 
+import static java.lang.Double.NaN;
 import static java.lang.Math.*;
 
 import org.osm2world.core.math.VectorXZ;
+import org.osm2world.core.osm.data.OSMData;
 
 /**
  * application of an orthographic projection that is intended to use
@@ -14,10 +16,33 @@ import org.osm2world.core.math.VectorXZ;
 public class OrthographicAzimuthalMapProjection extends OriginMapProjection {
 	
 	private final double GLOBE_RADIUS = 6371000;
-			
+	
+	private double lat0 = NaN;
+	private double lon0 = NaN;
+	
 	@Override
 	public VectorXZ calcPos(LatLon latlon) {
 		return calcPos(latlon.lat, latlon.lon);
+	}
+	
+	@Override
+	public void setOrigin(LatLon origin) {
+		
+		super.setOrigin(origin);
+		
+		lat0 = toRadians(getOrigin().lat);
+		lon0 = toRadians(getOrigin().lon);
+		
+	}
+	
+	@Override
+	public void setOrigin(OSMData osmData) {
+		
+		super.setOrigin(osmData);
+		
+		lat0 = toRadians(getOrigin().lat);
+		lon0 = toRadians(getOrigin().lon);
+		
 	}
 	
 	@Override
@@ -26,8 +51,8 @@ public class OrthographicAzimuthalMapProjection extends OriginMapProjection {
 		double lat = toRadians(latDeg);
 		double lon = toRadians(lonDeg);
 		
-		double x = GLOBE_RADIUS * cos(lat) * sin(lon - origin.lon);
-		double y = GLOBE_RADIUS * (cos(origin.lat) * sin(lat) - sin(origin.lat) * cos(lat) * cos(lon - origin.lon));
+		double x = GLOBE_RADIUS * cos(lat) * sin(lon - lon0);
+		double y = GLOBE_RADIUS * (cos(lat0) * sin(lat) - sin(lat0) * cos(lat) * cos(lon - lon0));
 		
 		return new VectorXZ(x, y);
 		
@@ -40,9 +65,9 @@ public class OrthographicAzimuthalMapProjection extends OriginMapProjection {
 		double c = asin(rho / GLOBE_RADIUS);
 		
 		if (rho > 0) {
-			return asin( cos(c) * sin(origin.lat) + ( pos.z * sin(c) * cos(origin.lat) ) / rho );
+			return toDegrees(asin( cos(c) * sin(lat0) + ( pos.z * sin(c) * cos(lat0) ) / rho ));
 		} else {
-			return origin.lat;
+			return toDegrees(lat0);
 		}
 		
 	}
@@ -53,12 +78,12 @@ public class OrthographicAzimuthalMapProjection extends OriginMapProjection {
 		double rho = sqrt(pos.x * pos.x + pos.z * pos.z);
 		double c = asin(rho / GLOBE_RADIUS);
 		
-		double div = rho * cos(origin.lat) * cos(c) - pos.z * sin(origin.lat) * sin(c);
+		double div = rho * cos(lat0) * cos(c) - pos.z * sin(lat0) * sin(c);
 		
 		if (abs(div) > 1e-5) {
-			return origin.lon + atan2( pos.x * sin(c), div );
+			return toDegrees(lon0 + atan2( pos.x * sin(c), div ));
 		} else {
-			return origin.lon;
+			return toDegrees(lon0);
 		}
 		
 	}
