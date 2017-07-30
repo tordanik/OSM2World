35f9c9b969f61bc72fd4090bead8ff7bb1d357d6
Tobias Knerr <osm@tobias-knerr.de>
2017-07-30 14:32:10 +0200

    update exception handling in JTSConversionUtil

--- src/org/osm2world/core/math/JTSConversionUtil.java
+++ src/org/osm2world/core/math/JTSConversionUtil.java
@@ -70,8 +70,7 @@ public class JTSConversionUtil {
 	}
 	
 	public static final PolygonWithHolesXZ
-		polygonXZFromJTSPolygon(Polygon polygon)
-		throws InvalidGeometryException {
+		polygonXZFromJTSPolygon(Polygon polygon) {
 		
 		/* create outer polygon */
 		
@@ -113,13 +112,7 @@ public class JTSConversionUtil {
 		
 		if (geometry instanceof Polygon) {
 			if (geometry.getNumPoints() > 2) {
-				
-				try {
-					result.add(polygonXZFromJTSPolygon((Polygon)geometry));
-				} catch (InvalidGeometryException e) {
-					//don't add the broken polygon
-				}
-				
+				result.add(polygonXZFromJTSPolygon((Polygon)geometry));
 			}
 		} else if (geometry instanceof GeometryCollection) {
 			GeometryCollection collection = (GeometryCollection)geometry;
