3a7024f238434793196d0a925cfe13860b993607
Tobias Knerr <osm@tobias-knerr.de>
2021-07-24 15:30:01 +0200

    Improve unit tests for map projections

--- src/test/java/org/osm2world/core/map_data/creation/MetricMapProjectionTest.java
+++ src/test/java/org/osm2world/core/map_data/creation/MetricMapProjectionTest.java
@@ -1,6 +1,10 @@
 package org.osm2world.core.map_data.creation;
 
-public class MetricMapProjectionTest {
+public class MetricMapProjectionTest extends AbstractMapProjectionTest {
 
+	@Override
+	protected MapProjection createProjection(LatLon origin) {
+		return new MetricMapProjection(origin);
+	}
 
 }
--- src/test/java/org/osm2world/core/map_data/creation/OrthographicAzimuthalMapProjectionTest.java
+++ src/test/java/org/osm2world/core/map_data/creation/OrthographicAzimuthalMapProjectionTest.java
@@ -1,32 +1,37 @@
 package org.osm2world.core.map_data.creation;
 
+import static org.junit.Assert.assertEquals;
 import static org.osm2world.core.test.TestUtil.assertAlmostEquals;
 
 import org.junit.Test;
 import org.osm2world.core.math.VectorXZ;
 
+public class OrthographicAzimuthalMapProjectionTest extends AbstractMapProjectionTest {
 
-public class OrthographicAzimuthalMapProjectionTest {
+	@Override
+	protected MapProjection createProjection(LatLon origin) {
+		return new OrthographicAzimuthalMapProjection(origin);
+	}
 
 	@Test
-	public void testCalcPos() {
+	public void testToXZ() {
 
-		OrthographicAzimuthalMapProjection proj = new OrthographicAzimuthalMapProjection(new LatLon(0, 0));
+		MapProjection proj = createProjection(new LatLon(0, 0));
 
 		VectorXZ posOrigin = proj.toXZ(0, 0);
 		assertAlmostEquals(0, 0, posOrigin);
-		assertAlmostEquals(0, proj.toLat(posOrigin));
-		assertAlmostEquals(0, proj.toLon(posOrigin));
+		assertEquals(0, proj.toLat(posOrigin), DELTA);
+		assertEquals(0, proj.toLon(posOrigin), DELTA);
 
 		VectorXZ posE = proj.toXZ(0, 0.000009);
 		assertAlmostEquals(1, 0, posE);
-		assertAlmostEquals(0, proj.toLat(posE));
-		assertAlmostEquals(0.000009, proj.toLon(posE));
+		assertEquals(0, proj.toLat(posE), DELTA);
+		assertEquals(0.000009, proj.toLon(posE), DELTA);
 
 		VectorXZ posN = proj.toXZ(0.000009, 0);
 		assertAlmostEquals(0, 1, posN);
-		assertAlmostEquals(0.000009, proj.toLat(posN));
-		assertAlmostEquals(0, proj.toLon(posN));
+		assertEquals(0.000009, proj.toLat(posN), DELTA);
+		assertEquals(0, proj.toLon(posN), DELTA);
 
 	}
 
