57571395d4f631069ede13bf67bdf62bd9002315
Tobias Knerr <osm@tobias-knerr.de>
2023-02-24 14:03:37 +0100

    Add unit test for JOSM XML files with emojis

--- src/test/java/org/osm2world/core/osm/creation/OSMFileReaderTest.java
+++ src/test/java/org/osm2world/core/osm/creation/OSMFileReaderTest.java
@@ -1,18 +1,18 @@
 package org.osm2world.core.osm.creation;
 
-import static de.topobyte.osm4j.core.model.util.OsmModelUtil.*;
-import static org.junit.Assert.*;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.junit.Test;
-import org.osm2world.core.osm.data.OSMData;
-
 import de.topobyte.osm4j.core.model.iface.OsmNode;
 import de.topobyte.osm4j.core.model.iface.OsmRelation;
 import de.topobyte.osm4j.core.model.iface.OsmWay;
 import de.topobyte.osm4j.core.resolve.EntityNotFoundException;
+import org.junit.Test;
+import org.osm2world.core.osm.data.OSMData;
+
+import java.io.File;
+import java.io.IOException;
+
+import static de.topobyte.osm4j.core.model.util.OsmModelUtil.getTagsAsMap;
+import static de.topobyte.osm4j.core.model.util.OsmModelUtil.nodesAsList;
+import static org.junit.Assert.*;
 
 
 public class OSMFileReaderTest {
@@ -36,6 +36,18 @@ public class OSMFileReaderTest {
 
 		OsmRelation relation = osmData.getRelations().iterator().next();
 		assertEquals("associatedStreet",  getTagsAsMap(relation).get("type"));
+
+	}
+
+	@Test
+	public void testJosmFileWithEmoji() throws IOException, EntityNotFoundException {
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		File testFile = new File(classLoader.getResource("josm_emoji.osm").getFile());
+		OSMData osmData = new OSMFileReader(testFile).getData();
+
+		assertSame(1, osmData.getNodes().size());
+		assertNotNull(osmData.getNode(123123123123L));
 
 	}
 
