cc048e845896fbe39a095b0d928381497840b181
Tobias Knerr <osm@tobias-knerr.de>
2023-05-11 00:04:53 +0200

    Validate LatLon objects during constructor call

--- src/main/java/org/osm2world/core/map_data/creation/LatLon.java
+++ src/main/java/org/osm2world/core/map_data/creation/LatLon.java
@@ -1,15 +1,18 @@
 package org.osm2world.core.map_data.creation;
 
-import org.osm2world.console.LatLonEle;
-
 import java.util.Objects;
+
+import org.osm2world.console.LatLonEle;
 
 /**
  * an immutable coordinate pair with latitude and longitude
  */
 public class LatLon {
 
+	/** latitude in degrees */
 	public final double lat;
+
+	/** longitude in degrees */
 	public final double lon;
 
 	// typographical minus '−' works around the CLI parser's special handling of '-'
@@ -21,6 +24,7 @@ public class LatLon {
 	public LatLon(double lat, double lon) {
 		this.lat = lat;
 		this.lon = lon;
+		validateValues();
 	}
 
 	/** parsing constructor for strings matching {@link #PATTERN} */
@@ -28,6 +32,16 @@ public class LatLon {
 		LatLonEle lle = new LatLonEle(string);
 		this.lat = lle.lat;
 		this.lon = lle.lon;
+		validateValues();
+	}
+
+	/**
+	 * @throws IllegalArgumentException  for incorrect field values
+	 */
+	private void validateValues() {
+		if (lat > 90 || lat < -90 || lon > 180 || lon < -180) {
+			throw new IllegalArgumentException("Latitude or longitude not valid: " + lat + ", " + lon);
+		}
 	}
 
 	@Override
