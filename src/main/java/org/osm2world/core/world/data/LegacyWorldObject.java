1ea16b09e7006e9ed90e34ab8524b2721eccf863
Tobias Knerr <osm@tobias-knerr.de>
2023-07-16 18:28:53 +0200

    Allow LegacyWorldObjects to define LOD

--- src/main/java/org/osm2world/core/world/data/LegacyWorldObject.java
+++ src/main/java/org/osm2world/core/world/data/LegacyWorldObject.java
@@ -2,9 +2,11 @@ package org.osm2world.core.world.data;
 
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.osm2world.core.target.Renderable;
 import org.osm2world.core.target.Target;
 import org.osm2world.core.target.common.MeshTarget;
+import org.osm2world.core.target.common.mesh.LevelOfDetail;
 import org.osm2world.core.target.common.mesh.Mesh;
 
 /**
@@ -18,7 +20,14 @@ public interface LegacyWorldObject extends WorldObject, Renderable {
 	default List<Mesh> buildMeshes() {
 		MeshTarget meshTarget = new MeshTarget();
 		renderTo(meshTarget);
-		return meshTarget.getMeshes();
+		return meshTarget.getMeshes().stream()
+				.map(mesh -> new Mesh(mesh.geometry, mesh.material, getLodRange().getLeft(), getLodRange().getRight()))
+				.toList();
+	}
+
+	/** returns a pair of min and max LOD for this {@link LegacyWorldObject}'s implicit {@link Mesh}es */
+	default Pair<LevelOfDetail, LevelOfDetail> getLodRange() {
+		return Pair.of(LevelOfDetail.LOD0, LevelOfDetail.LOD4);
 	}
 
 }
