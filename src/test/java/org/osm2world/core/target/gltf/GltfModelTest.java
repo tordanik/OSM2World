4437d813109a372eacdec5f304d5c92c4de4f03b
Tobias Knerr <osm@tobias-knerr.de>
2024-10-07 23:21:12 +0200

    Add BoxVertexColors test case for glTF import

--- src/test/java/org/osm2world/core/target/gltf/GltfModelTest.java
+++ src/test/java/org/osm2world/core/target/gltf/GltfModelTest.java
@@ -1,11 +1,17 @@
 package org.osm2world.core.target.gltf;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.osm2world.core.target.gltf.GltfModel.readComponent;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.osm2world.core.math.VectorXYZ;
+import org.osm2world.core.target.common.model.InstanceParameters;
 import org.osm2world.core.target.gltf.data.GltfAccessor;
 
 public class GltfModelTest {
@@ -30,6 +36,23 @@ public class GltfModelTest {
 		assertEquals(4242, readComponent(b, GltfAccessor.TYPE_UNSIGNED_SHORT, false), 0.001f);
 		assertEquals(42424242, readComponent(b, GltfAccessor.TYPE_UNSIGNED_INT, false), 0.001f);
 		assertEquals(-42.3f, readComponent(b, GltfAccessor.TYPE_FLOAT, false), 0.001f);
+
+	}
+
+	@Ignore
+	@Test
+	public void testLoadFromFile_BoxVertexColors() throws IOException {
+
+		String assetName = "BoxVertexColors";
+		String fileName = "gltf" + File.separator + assetName + File.separator + "BoxVertexColors.gltf";
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		File testFile = new File(classLoader.getResource(fileName).getFile());
+
+		var model = GltfModel.loadFromFile(testFile);
+		var meshes = model.buildMeshes(new InstanceParameters(VectorXYZ.NULL_VECTOR, 0));
+
+		assertFalse(meshes.isEmpty());
 
 	}
 
