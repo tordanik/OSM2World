39a7c7d5fe7c246a1ca085ac46421dab7207a664
Tobias Knerr <osm@tobias-knerr.de>
2024-10-09 13:21:26 +0200

    Add and enable the BoxVertexColors test cases

--- src/test/java/org/osm2world/core/target/gltf/GltfModelTest.java
+++ src/test/java/org/osm2world/core/target/gltf/GltfModelTest.java
@@ -1,14 +1,13 @@
 package org.osm2world.core.target.gltf;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.osm2world.core.target.gltf.GltfModel.readComponent;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.osm2world.core.math.VectorXYZ;
 import org.osm2world.core.target.common.model.InstanceParameters;
@@ -39,20 +38,30 @@ public class GltfModelTest {
 
 	}
 
-	@Ignore
 	@Test
 	public void testLoadFromFile_BoxVertexColors() throws IOException {
 
-		String assetName = "BoxVertexColors";
-		String fileName = "gltf" + File.separator + assetName + File.separator + "BoxVertexColors.gltf";
+		for (String extension : List.of(".gltf", "_embedded.gltf")) {
+
+			var model = loadGltfTestModel("BoxVertexColors", extension);
+			var meshes = model.buildMeshes(new InstanceParameters(VectorXYZ.NULL_VECTOR, 0));
+
+			assertEquals(1, meshes.size());
+			assertEquals(12, meshes.get(0).geometry.asTriangles().triangles.size());
+
+		}
+
+	}
+
+	private static GltfModel loadGltfTestModel(String assetName, String extension) throws IOException {
+
+		String fileName = "gltf" + File.separator + assetName + File.separator + assetName + extension;
 
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		File testFile = new File(classLoader.getResource(fileName).getFile());
 
 		var model = GltfModel.loadFromFile(testFile);
-		var meshes = model.buildMeshes(new InstanceParameters(VectorXYZ.NULL_VECTOR, 0));
-
-		assertFalse(meshes.isEmpty());
+		return model;
 
 	}
 
