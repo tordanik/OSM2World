7743a4761bb89182b8a0e3c0a2a421bc2b546bad
Tobias Knerr <osm@tobias-knerr.de>
2024-10-25 13:56:37 +0200

    Implement MeshHeightAndSizeComparator using comparingInt

--- src/main/java/org/osm2world/core/target/gltf/MeshHeightAndSizeComparator.java
+++ src/main/java/org/osm2world/core/target/gltf/MeshHeightAndSizeComparator.java
@@ -5,35 +5,20 @@ import java.util.Comparator;
 import org.osm2world.core.math.AxisAlignedRectangleXZ;
 import org.osm2world.core.target.common.MeshStore;
 import org.osm2world.core.target.common.MeshStore.MeshWithMetadata;
-import org.osm2world.core.target.gltf.GltfTarget.MinMax;
 
 final class MeshHeightAndSizeComparator implements Comparator<MeshStore.MeshWithMetadata> {
+
+	private final Comparator<MeshStore.MeshWithMetadata> IMPLEMENTATION = Comparator.comparingInt((MeshStore.MeshWithMetadata m) -> {
+		// TODO: does not work with terrain enabled, a small object in a high location will have large y values
+		double max = m.mesh().geometry.asTriangles().vertices().stream().mapToDouble(v -> v.y).max().orElse(0);
+		return (int) Math.round(max * 2) / 2;
+	}).thenComparingInt(m -> {
+		AxisAlignedRectangleXZ bbox = AxisAlignedRectangleXZ.bbox(m.mesh().geometry.asTriangles().vertices());
+		return (int) Math.round(bbox.getDiameter() * 4) / 4;
+	}).reversed();
+
 	@Override
 	public int compare(MeshWithMetadata m1, MeshWithMetadata m2) {
-		MinMax ymm1 = new MinMax();
-		MinMax ymm2 = new MinMax();
-
-		m1.mesh().geometry.asTriangles().vertices().forEach(v -> {
-			ymm1.max = Double.max(ymm1.max, v.y);
-		});
-		
-		m2.mesh().geometry.asTriangles().vertices().forEach(v -> {
-			ymm2.max = Double.max(ymm2.max, v.y);
-		});
-
-		int h1 = (int)Math.round(ymm1.max * 2) / 2;
-		int h2 = (int)Math.round(ymm2.max * 2) / 2;
-
-		if (h2 != h1) {
-			return h2 - h1;
-		}
-
-		AxisAlignedRectangleXZ bbx1 = AxisAlignedRectangleXZ.bbox(m1.mesh().geometry.asTriangles().vertices());
-		AxisAlignedRectangleXZ bbx2 = AxisAlignedRectangleXZ.bbox(m2.mesh().geometry.asTriangles().vertices());
-
-		int d1 = (int)Math.round(bbx1.getDiameter() * 4) / 4;
-		int d2 = (int)Math.round(bbx2.getDiameter() * 4) / 4;
-
-		return d2 - d1;
+		return IMPLEMENTATION.compare(m1, m2);
 	}
 }
