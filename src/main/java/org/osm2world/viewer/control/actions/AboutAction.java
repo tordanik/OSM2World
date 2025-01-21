e3a2a5bc73a91bb30fda1b20ab9c84da61bfcaac
Tobias Knerr <osm@tobias-knerr.de>
2025-01-21 12:01:25 +0100

    Improve the About window with clickable links and styling

--- src/main/java/org/osm2world/viewer/control/actions/AboutAction.java
+++ src/main/java/org/osm2world/viewer/control/actions/AboutAction.java
@@ -1,10 +1,12 @@
 package org.osm2world.viewer.control.actions;
 
+import static org.osm2world.core.GlobalValues.*;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.JOptionPane;
-import static org.osm2world.core.GlobalValues.*;
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
 
 public class AboutAction extends AbstractAction {
 
@@ -16,10 +18,42 @@ public class AboutAction extends AbstractAction {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		JOptionPane.showMessageDialog(null, "OSM2World\nVersion " + VERSION_STRING
-				+ "\n\nFor more information, visit\n"
-				+ WIKI_URI + "\n" + OSM2WORLD_URI,
-				"About OSM2World", JOptionPane.INFORMATION_MESSAGE);
+
+		JDialog dialog = new JDialog();
+		dialog.setTitle("About OSM2World");
+		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		dialog.add(createAboutContent(), BorderLayout.CENTER);
+		dialog.setSize(800, 600);
+		dialog.setLocationRelativeTo(null);
+		dialog.setVisible(true);
+
+	}
+
+	private static JScrollPane createAboutContent() {
+
+		var tabText = new JEditorPane();
+		tabText.setContentType("text/html");
+		tabText.setText("<html><body style='padding: 10px;'>"
+				+ "<h1>OSM2World</h1>"
+				+ "<p>Version " + VERSION_STRING + "</p>"
+				+ "<br/><table>"
+				+ "<tr><td>Website<td><a href='" + OSM2WORLD_URI + "'>" + OSM2WORLD_URI.replace("https://", "") + "</a>"
+				+ "<tr><td>Wiki<td><a href='" + WIKI_URI + "'>" + WIKI_URI.replace("https://", "") + "</a><br>"
+				+ "<tr><td>Forum<td><a href='https://community.osm.org/tag/osm2world'>community.osm.org/tag/osm2world</a><br>"
+				+ "<tr><td>Issues<td><a href='https://github.com/tordanik/OSM2World/issues'>github.com/tordanik/OSM2World/issues</a><br>"
+				+ "</table></p></body></html>");
+		tabText.setEditable(false);
+		tabText.setOpaque(false);
+		tabText.addHyperlinkListener(e -> {
+			if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+				try {
+					Desktop.getDesktop().browse(e.getURL().toURI());
+				} catch (Exception ignored) {}
+			}
+		});
+
+		return new JScrollPane(tabText);
+
 	}
 
 }
