67a50088fd2ad4b40fde897537bdc80d78bd8334
Tobias Knerr <osm@tobias-knerr.de>
2025-01-21 12:35:47 +0100

    Add a tab with dependency licenses to the About window

--- src/main/java/org/osm2world/viewer/control/actions/AboutAction.java
+++ src/main/java/org/osm2world/viewer/control/actions/AboutAction.java
@@ -4,9 +4,17 @@ import static org.osm2world.core.GlobalValues.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class AboutAction extends AbstractAction {
 
@@ -22,18 +30,23 @@ public class AboutAction extends AbstractAction {
 		JDialog dialog = new JDialog();
 		dialog.setTitle("About OSM2World");
 		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		dialog.add(createAboutContent(), BorderLayout.CENTER);
+
+		var tabbedPane = new JTabbedPane();
+		tabbedPane.addTab("About", createAboutTabContent());
+		tabbedPane.addTab("Third-party licenses", createLicenseTabContent());
+
+		dialog.setLayout(new BorderLayout());
+		dialog.add(tabbedPane, BorderLayout.CENTER);
+
 		dialog.setSize(800, 600);
 		dialog.setLocationRelativeTo(null);
 		dialog.setVisible(true);
 
 	}
 
-	private static JScrollPane createAboutContent() {
+	private static JComponent createAboutTabContent() {
 
-		var tabText = new JEditorPane();
-		tabText.setContentType("text/html");
-		tabText.setText("<html><body style='padding: 10px;'>"
+		String text = "<html><body style='padding: 10px;'>"
 				+ "<h1>OSM2World</h1>"
 				+ "<p>Version " + VERSION_STRING + "</p>"
 				+ "<br/><table>"
@@ -41,7 +54,86 @@ public class AboutAction extends AbstractAction {
 				+ "<tr><td>Wiki<td><a href='" + WIKI_URI + "'>" + WIKI_URI.replace("https://", "") + "</a><br>"
 				+ "<tr><td>Forum<td><a href='https://community.osm.org/tag/osm2world'>community.osm.org/tag/osm2world</a><br>"
 				+ "<tr><td>Issues<td><a href='https://github.com/tordanik/OSM2World/issues'>github.com/tordanik/OSM2World/issues</a><br>"
-				+ "</table></p></body></html>");
+				+ "</table></p></body></html>";
+
+		return createReadonlyHtmlComponent(text);
+
+	}
+
+	private static JComponent createLicenseTabContent() {
+
+		String attributionText;
+
+		try {
+
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			URL resource = classLoader.getResource("attribution.xml");
+			if (resource != null) {
+
+				var attributionFile = new File(resource.getFile());
+
+				var attributionTextBuilder = new StringBuilder();
+				attributionTextBuilder.append("<html><body style='padding: 10px;'>");
+
+				Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(attributionFile);
+				document.getDocumentElement().normalize();
+
+				// Get all <dependency> elements
+				NodeList dependencyNodes = document.getElementsByTagName("dependency");
+				for (int i = 0; i < dependencyNodes.getLength(); i++) {
+					Node node = dependencyNodes.item(i);
+					if (node.getNodeType() == Node.ELEMENT_NODE && node instanceof Element element) {
+
+						Node name = element.getElementsByTagName("name").item(0);
+						Node version = element.getElementsByTagName("version").item(0);
+						Node url = element.getElementsByTagName("projectUrl").item(0);
+						NodeList licenses = element.getElementsByTagName("license");
+
+						if (name != null && licenses.getLength() > 0) {
+
+							attributionTextBuilder.append("<h2>").append(name.getTextContent());
+							if (version != null) {
+								attributionTextBuilder.append(" ").append(version.getTextContent());
+							}
+							attributionTextBuilder.append("</h2>");
+
+							if (url != null) {
+								attributionTextBuilder.append("<p>").append(url.getTextContent()).append("</p>");
+							}
+
+							for (int licenseIndex = 0; licenseIndex < licenses.getLength(); licenseIndex++) {
+								Node license = licenses.item(licenseIndex);
+								attributionTextBuilder.append("<p>").append(license.getTextContent()).append("</p>");
+							}
+
+							attributionTextBuilder.append("<br/>");
+
+						}
+
+					}
+				}
+
+				attributionTextBuilder.append("</body></html>");
+				attributionText = attributionTextBuilder.toString();
+
+			} else {
+				throw new Exception("Could not load resource");
+			}
+		} catch (Exception e) {
+			attributionText = "Could not read license information: <br/>" + e.getMessage();
+		}
+
+		return createReadonlyHtmlComponent(attributionText);
+
+	}
+
+	/** returns a component showing scrollable, read-only HTML text with clickable links */
+	private static JComponent createReadonlyHtmlComponent(String text) {
+
+		var tabText = new JEditorPane();
+		tabText.setContentType("text/html");
+		tabText.setText(text);
+		tabText.setCaretPosition(0);
 		tabText.setEditable(false);
 		tabText.setOpaque(false);
 		tabText.addHyperlinkListener(e -> {
